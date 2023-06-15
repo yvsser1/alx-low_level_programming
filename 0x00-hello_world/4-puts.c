4b0de342c495e60b3b3d49f82a74cebe782dd3dd
Yasser <yasserdalouzi@gmail.com>
2023-06-15 11:26:19 +0100

    Typo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-	puts( "\"Programming is like building a multilingual puzzle")
+	puts( "\"Programming is like building a multilingual puzzle");
 	
 	return 0;
 }
