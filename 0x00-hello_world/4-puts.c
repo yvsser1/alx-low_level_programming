8403b45bc4dca9c0557e4a97f296741ca23e32cf
Yasser <yasserdalouzi@gmail.com>
2023-06-15 11:37:34 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+*main - Main Entry function of the program
+*
+*
+*Return: No Return Void
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
