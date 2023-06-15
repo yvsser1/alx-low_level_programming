21cd0ccea927cf933852b4d8da029194443be02a
Yasser <yasserdalouzi@gmail.com>
2023-06-15 11:37:42 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+*main - Main Entry function of the program
+*
+*
+*Return: No Return Void
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
