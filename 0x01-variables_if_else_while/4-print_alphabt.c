f16bef5ba24607d5a025e1ed9624b52d36c11508
Yasser <yasserdalouzi@gmail.com>
2023-06-19 21:15:50 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
-*main - Program to print alphabet letters except q and e followed by new line
+*main - Prints alphabet letters except q and e followed by new line
 *
 *Return: return 0
 */
