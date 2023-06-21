eeb9de54ac0a93a33f16e3d4587b90404e4d8aed
Yasser <yasserdalouzi@gmail.com>
2023-06-21 22:44:12 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * main function
- * Return: 0
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
