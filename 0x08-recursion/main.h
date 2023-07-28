6aee99df3dc68b861af2124392bc99f0472b7729
yvsser1 <yasserdalouzi@gmail.com>
2023-07-28 09:39:13 -0700

    0x08 Progress

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char) 
+*/
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif /* MAIN_H */
